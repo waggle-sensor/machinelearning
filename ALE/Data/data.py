d0372f0067ca6a30282551886868a2f5088f8806
EnriqueNueve <enriquenueve9@gmail.com>
2020-09-24 13:59:15 -0500

    Add files via upload

--- ALE/Data/data.py
+++ ALE/Data/data.py
@@ -1,7 +1,26 @@
 import os
+import shutil
 import pandas as pd
 import numpy as np
 from math import floor
+
+#######################################################
+
+"""
+
+How to make a new data set: 
+1. Obtain new data (case dependent)
+2. Place raw orginal data in folder named "abc" (abc is an example name, make it whatever you want)
+    within Data/DataSets/abc/raw_data
+3. Create two csv files: 
+        I. data_tab.csv whose columns are unique ids that match to each sample and each sample's target values
+        II. a csv file named abc.csv that has each samples id and also input values in each row (in cases where
+                input data can not be formatted in such a manner, place raw data in folder within raw_data named 
+                abc where each individual data sample is named it's respective id
+
+
+
+"""
 
 
 #######################################################
@@ -14,22 +33,90 @@ def load_data_py():
 #######################################################
 
 class DataManager:
-    # Class variables
-    # z = 1
+    """
+    DataManger Documentation:
+    --------------------------
 
-    def __init__(self, dataset_name, bins=1):
+    Purpose
+    ----------
+    DataManger is the parent class for custom and pre-made data loaders. DataManger serves
+    critical purposes: check if requested data is present in Data/Datasets, make new data
+    from original data to be used for Active Learning (initial training data, validation data,
+    unlabeled samples (could be one cache of unlabeled data or multiple sub classes of unlabeled data), and
+    track what samples are removed and replaced in different caches based on the Active Learning algorithms
+    choices.
 
-        assert isinstance(dataset_name, str), "dataset_name must be a string"
-        self.dataset_name = dataset_name
+    Arguments
+    _________
+    dataset_name : str
+        dataset_name is a string that is used to select what data to be used by DataManager. In __init__ section
+        B., logic is shown on how DataManger checks if a data set is present and formatted in the proper way.
+
+    bins : int
+        bins is a int that states how many unique caches should be made for the unlabeled data. For example,
+        say that there are 300 unlabeled examples and bins = 3. Then three unlabeled caches (u_cache_0, u_cache_1,
+        u_cache_2) will be made each containing one hundred unique samples from the larger set of 300 unlabeled
+        samples.
+
+    keep_bins : bool
+        keep_bins is a bool that determines whether or not to place unselected values from current round
+        into the next round's pool. For example, say current round's pool is [a,b,d] and next round's pool
+        is [d,e,f]. Say keep_bins==True and 'a' was chosen, then the next round's pool would be updated to
+        [b,c,d,e,f].
+
+    Attributes
+    ----------
+    data_path : str
+        path to folder where data is stored within ./Data/
+
+    u_cache : list
+        list to store all ids of samples
+
+    train_cache : list
+        list to store ids of what samples belong to the training set
+
+    val_cache : list
+        list to store ids of what samples belong to the validation set
+
+    unlabeled_cache : list
+        list to store ids of what samples belong to the unlabeled set that can be selected from during
+        active learning. NOTE! unlabeled cache can be a list of list where the sublist are the cache ids
+        that are produced from bins > 1 where multiple unlabeled caches are made.
+
+    Methods
+    -------
+    clearCache(self):
+        This method sets u_cache, train_cache, val_cache, and unlabeled_cache back to empty list []
+
+    checkDataSet(self):
+       Checks if passed data set name has respective folder
+
+    parseData(self, split, bins=1, keep_bins=False):
+        Makes train, validation, and unlabeled sampling cache csv files that contain
+        respective sample ids
+
+    loadCaches(self):
+        Reads each cache's csv file and stores respective ids in train_cache, val_cache, unlabeled_cache
+
+    deleteCache(self):
+        Deletes each cache's csv files. This may be needed if you desire to re-run an test from scratch
+
+    """
+
+    def __init__(self, dataset_name, bins=1, keep_bins=None):
+
+        # A. Fields of DataManger class
         self.n_total_samples = 0
         self.bins = bins  # Number of unlabeled caches corresponding to each round of retraining
-        self.keep_bins = False
+        self.keep_bins = keep_bins  # Push previous rounds unused samples to next round
 
-        # Check if data set is in Data/DataSet
+        # B. Check if data set is in Data/DataSet
+        assert isinstance(dataset_name, str), "dataset_name must be a string"
+        self.dataset_name = dataset_name
         self.checkDataSet()
         self.data_path = "Data/DataSets/" + self.dataset_name
 
-        # List to keep track of what data is used where
+        # C. List to keep track of what data is used where
         self.u_cache = []  # Keeps running track of all data
         self.train_cache = []  # Keeps track of what the model is allowed to see during training
         self.val_cache = []  # Keep track of what data to use for validation
@@ -84,7 +171,8 @@ class DataManager:
             exit()
 
         # Set keep_bins as bool passed
-        self.keep_bins = keep_bins
+        if self.keep_bins == None:
+            self.keep_bins = keep_bins
 
         # Open data tab
         self.data_tab = pd.read_csv(self.data_path + "/raw_data/data_tab.csv")
@@ -183,8 +271,21 @@ class DataManager:
         temp_cache_df = pd.read_csv(self.data_path + "/val_cache_" + ".csv")
         self.val_cache = temp_cache_df["ID"].tolist()
 
-    def sampleUCache(self, method, bin_n=None):
-        pass
+    def deleteCache(self):
+        """
+        Delete cache (train, val, and ul_cache)
+        """
+        # Delete unlabeled cache csv
+        if self.bins == 1:
+            os.remove(self.data_path + "/ul_cache" + ".csv")
+        else:
+            shutil.rmtree(self.data_path + "/ul_cache")
+
+        # Delete train cache csv
+        os.remove(self.data_path + "/train_cache_" + ".csv")
+
+        # Delete val cache csv
+        os.remove(self.data_path + "/val_cache_" + ".csv")
 
 
 #######################################################
@@ -203,15 +304,60 @@ class CustomGenerator(DataManager):
 #######################################################
 
 class ToyALoader(DataManager):
-    def __init__(self, bins=1, keep_bins=False):
-        super().__init__("ToyA", bins)
+    """
+    ToyALoader Documentation:
+    --------------------------
 
-        self.keep_bins = keep_bins
-        self.loadCaches()
+    Purpose
+    ----------
+    Load data as needed from the ToyA dataset. Currently, I just load the whole
+    data set into memory. Not efficient at all yet, currently unaware of alternative
+    method. Need to keep looking into this issue. ToyA is a child instance of the class
+    DataManager. DataManager is disgustingly large and is a mental pain ... I'll go back and clean it up.
+
+    Attributes
+    ----------
+    data_df : pd.DataFrame()
+        Stores dataframe of ToyA data. This is unique to this class. Each child class of DataManager
+        is going to have a unique feature such as this due to using different types of data.
+
+    Methods
+    -------
+    getBatch(self, cache_ids: list):
+        grabs row of data from data_df based on id's in passed list. Converts into
+        numpy arrays. Makes input matrix and one hot encoded array as target value.
+        Returns processed input and target data.
+
+    """
+
+    def __init__(self, bins=1, keep_bins=False):
+        super().__init__("ToyA", bins, keep_bins)
+
+        # self.loadCaches()
+
+        # For this data set, it is possible to read in all data at one
         self.data_df = pd.read_csv(self.data_path + "/raw_data/toyA.csv", iterator=True, chunksize=5000)
         self.data_df = pd.concat(self.data_df, ignore_index=True)
 
     def getBatch(self, cache_ids: list):
+        """
+        Purpose
+        _______
+        Receives from algo class list of sample ids to get from memory or RAM
+
+        Arguments
+        __________
+        cache_ids : list
+            list contains ids of what data samples to send back
+
+        Returns
+        _______
+        X : np.array
+            Input values for model
+        y : Target values for model
+
+        """
+
         # Select data based off of passed cache_ids
         batchData = self.data_df.loc[self.data_df['ID'].isin(cache_ids)]
         y = batchData[["y"]].values
@@ -223,10 +369,9 @@ class ToyALoader(DataManager):
             if y[i] == -1:
                 y_onehot[i, 0] = 1
             else:
-                y_onehot[i,1] = 1
+                y_onehot[i, 1] = 1
 
         return X, y_onehot
-
 
 
 #######################################################
@@ -260,20 +405,39 @@ class mnistLoader(DataManager):
     """
 
     def __init__(self, bins=1, keep_bins=False):
-        super().__init__("MNIST", bins)
+        super().__init__("MNIST", bins, keep_bins)
 
-        self.keep_bins = keep_bins
         self.loadCaches()
+
+        # For this data set, it is possible to read in all data at one
         self.data_df = pd.read_csv(self.data_path + "/raw_data/mnist.csv", iterator=True, chunksize=5000)
         self.data_df = pd.concat(self.data_df, ignore_index=True)
 
     def getBatch(self, cache_ids: list):
+        """
+        Purpose
+        _______
+        Receives from algo class list of sample ids to get from memory or RAM
+
+        Arguments
+        __________
+        cache_ids : list
+            list contains ids of what data samples to send back
+
+        Returns
+        _______
+        X : np.array
+            Input values for model
+        y : Target values for model
+
+        """
+
         # Select data based off of passed cache_ids
         batchData = self.data_df.loc[self.data_df['ID'].isin(cache_ids)]
         y = batchData[["Label"]].values
         X = batchData.iloc[:, 2:].values
 
-        # Reshape input X
+        # Reshape input X into 2d array to express gray scaled image
         X = X.reshape(X.shape[0], 28, 28, 1)
         X = X / 255  # Scale input data between [0,1]
 
