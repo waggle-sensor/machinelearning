39301826c3a17a693dc42feb88069f6dbeec2d06
EnriqueNueve <enriquenueve9@gmail.com>
2020-09-25 14:25:48 -0500

    Add files via upload

--- ALE/Zoo/zoo.py
+++ ALE/Zoo/zoo.py
@@ -1,4 +1,7 @@
+# Import modules
 from importlib import import_module
+import abc
+import tensorflow as tf
 
 
 #######################################################
@@ -22,7 +25,8 @@ class zooKeeper():
             print("-" * 20)
 
             # TODO: make logic for if it is a pre-built class or not
-            modelClass = getattr(import_module("Zoo.Models." + self.modelName), self.modelName)
+            #modelClass = getattr(import_module("Zoo.Models." + self.modelName), self.modelName)
+            modelClass = getattr(import_module("Zoo." + self.modelName), self.modelName)
             self.modelObject = modelClass(loss=self.loss, optimizer=self.optimizer, metrics=self.metrics)
 
             # Display model layers, shapes, and number of parameters
@@ -37,27 +41,58 @@ class zooKeeper():
             raise
 
 
-"""
+#######################################################
 
-# Example class for models in Zoo/Models
-
-class tfModel():
+class customModel(metaclass=abc.ABCMeta):
     def __init__(self):
-        self.dummy = None
-     
-    def loadModel(self):
-        pass 
-    
-    def trainBatch(self):
-        pass
-    
-    def predict(self):
-        pass 
-    
-    def saveModel(self):
-        pass 
-    
-    def loadModel(self):
-        pass 
+        self.model = None
+        self.loss = None
+        self.opt = None
+        self.metrics = None
 
-"""
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (hasattr(subclass, 'loadModel') and
+                callable(subclass.loadModel) or
+                NotImplemented)
+
+    def testLoad(self):
+        print("-" * 20)
+        print("Model class successfully loaded!")
+        print("-" * 20)
+
+    @abc.abstractmethod
+    def loadModel(self) -> tf.keras.Sequential():
+        raise NotImplementedError
+
+    def grad(self, inputs, targets):
+        with tf.GradientTape() as tape:
+            loss_value = self.loss(self.model(inputs, training=True), targets)
+        return loss_value, tape.gradient(loss_value, self.model.trainable_variables)
+
+    def trainBatch(self, inputs, targets) -> float:
+        """ Calculates loss and gradients for batch of data and applies update to weights """
+        loss_value, grads = self.grad(inputs, targets)
+        self.opt.apply_gradients(zip(grads, self.model.trainable_variables))
+        return loss_value
+
+    def predict(self, inputs):
+        """ Used for predicting with model but does not have labels """
+        yh = self.model(inputs)
+        return yh
+
+    def getMetrics(self, yh, targets):
+        scores = {}
+        for i, metric in enumerate(self.metrics):
+            metric.reset_states()
+            metric.update_state(yh, targets)
+            scores[metric.name] = metric.result().numpy()
+            metric.reset_states()
+        return scores
+
+    def eval(self, inputs, targets):
+        """ Used for predicting with model and checking error when labels are provided """
+        yh = self.predict(inputs)
+        loss_value = self.loss(yh, targets)
+        metric_scores = self.getMetrics(yh, targets)
+        return loss_value, metric_scores, yh
