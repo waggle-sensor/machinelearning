d3a481fa93d3971581f9afc469b5bf069c7ec0d0
EnriqueNueve <enriquenueve9@gmail.com>
2020-09-25 09:41:48 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# machinelearning
+# ALE: Active Learning Exploratorium
+
+ALE is a Python library for testing Active Learning algorithims.
+
+## TODO
+* Implement inheritance over custom classes to ensure user safety
+    ** model class
+* Make acc work for get metric, need to round yh to one hot vector
+* Make util to test model and algo compatibility
+* Add controls for tracking and plotting metrics
+* Dynamically show changes in cache size to debug if 
+  cache is removing and adding data correctly ** saved in log, just need to display
+* Improve UI
+
+## DONE
+* Added ToyA dataset and ToyA_NN model
+* Implement engine.save_log(path="log.csv")
+* Add controls for saving performance and configuration
+
+## DataSets
+* MNIST
+* ToyA
+
+## Models
+* mnistCNN
+* ToyA_NN
+
+## Algos
+* Uniform sampling (i.e., passive learning, serves as baseline)
+* Least Confidence
+
+ Use the package manager pip to install WL
+```bash
+pip install ale
+```
+
+## Usage
+```python
+import ale
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ demo3.py
@@ -0,0 +1,89 @@
+"""
+
+Purpose: This script demonstrates using the pre-built mnist data set and loader, random
+sampling, and the pre-build mnistCNN network.
+
+To use: remove files and folders for mnist cache.
+"""
+
+from Data.data import *
+from AL import algos
+from engine import Engine
+from Zoo import zoo
+
+import tensorflow as tf
+
+
+#######################################################
+
+
+def main():
+    """
+    How to use:
+
+    # 1. Select data
+
+    # 2. Select Active Learning algorithm
+
+    # 3. Select model
+
+    # 4. Run algorithm and log results
+
+    """
+
+    # | ----------------------------
+    # | 1. Select data
+    # | ---------------------------
+
+    # DataManager parameters
+    split = (.05, .05, .9)  # (train, val, unlabeled)
+    bins = 1
+    keep_bins = False
+
+    dataClass = ToyALoader(bins, keep_bins)  # Declare data manager class
+    dataClass.parseData(split, bins, keep_bins)
+    dataClass.loadCaches()
+
+    # | ----------------------------
+    # | 2. Select Active Learning algorithm
+    # | ----------------------------
+
+    algo = algos.leastConfidence()  # Randomly selects samples from each round's cache
+    algo.reset()
+
+    # | ----------------------------
+    # | 3. Select model
+    # | ----------------------------
+
+    modelName = "ToyA_NN"  # Pick pre-made model
+    metrics = [tf.keras.metrics.MeanSquaredError(), tf.keras.metrics.KLDivergence()]
+    zk = zoo.zooKeeper(modelName, show_model=True, metrics=metrics)  # Load model and compile
+
+    # | ----------------------------
+    # | 4. Run algorithm and log results
+    # | ----------------------------
+
+    # Declare engine
+    sample_size = 50
+    engine = Engine(algo, dataClass, zk, sample_size)
+    input("press enter to continue")
+
+    # Initial training of model on original training data
+    engine.initialTrain(epochs=3, batch_size=16, val=True, plot=True)
+
+    # Run active learning algo
+    # Round is how many times the active learning algo samples
+    # cycles is how many epochs the model is retrained each time a round occurs of sampling
+    engine.run(rounds=3, cycles=1, batch_size=16, val=True, plot=True)
+    engine.saveLog(path="test_log.csv")
+    dataClass.deleteCache()
+
+    # | ----------------------------
+    # | Done
+    # | ----------------------------
+
+    print("Finished running")
+
+
+if __name__ == "__main__":
+    main()
